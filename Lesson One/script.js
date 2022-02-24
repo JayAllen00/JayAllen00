1f310ff3593ef4a49277b4a0cfba544477e7023e
JayAllen00 <99896718+JayAllen00@users.noreply.github.com>
2022-02-23 20:16:01 -0500

    inital commit

---
+++ Lesson One/script.js
@@ -0,0 +1,63 @@
+/**
+ * Create a travel bag object, with properties and some HTML to display the properties
+ */
+
+// Single line comment
+
+const updateTravelBag = (update) => { // creating an arrow function
+    let main = document.querySelector("main");
+    main.innerHTML = markup(travelBag);
+    console.info(update);
+};
+
+const travelBag = {
+    name: "Travel Duffle Bag",
+    volume: 52,
+    color: "bourbon",
+    dimensions: {
+        length: 22,
+        height: 22,
+        width: 12
+    },
+    box: {
+        name: "gift box",
+        color: "red",
+        shape: "square",
+        size: "small",
+        content: "watch"
+    },
+    zipperOpen: false,
+    toggleZipper: function(zipperStatus) {
+        this.zipperOpen = zipperStatus;
+    },
+    newDimensions: function(dimensionsLength, dimensionsHeight, dimensionsWidth){
+        this.dimensions.length = dimensionsLength;
+        this.dimensions.height = dimensionsHeight;
+        this.dimensions.width = dimensionsWidth;
+    },
+}
+
+const markup = (travelBag) => {
+    return `
+    <div>
+        <h3 style="color:red;">${travelBag.name}</h3>
+        <ul>
+        <li>Volume:${travelBag.volume}</li>
+        <li style="color:red;">Color:${travelBag.color}</li>
+        <li>Length:${travelBag.dimensions.length} inches</li>
+        <li>Height:${travelBag.dimensions.height}</li>
+        <li>Width:${travelBag.dimensions.width}</li>
+        <li>Bag contents:${travelBag.box.name}</li>
+        <li>Box color:${travelBag.box.color}</li>
+        <li>Box shape:${travelBag.box.shape}</li>
+        <li>Box size:${travelBag.box.size}</li>
+        <li>Box contents:${travelBag.box.content}</li>
+        <li>Zipper:${travelBag.zipperOpen ? "Open" : "Closed"}</li>
+        </ul>
+        <img src="images/TokyoSkytree.jpg" />
+    </div>    `
+}
+
+const main = document.createElement("main");
+main.innerHTML = markup(travelBag);
+document.body.appendChild(main); // drop the contents of the main element onto the page
